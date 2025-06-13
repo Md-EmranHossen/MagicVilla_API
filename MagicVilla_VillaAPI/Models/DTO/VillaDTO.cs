42470a5b4992787f90cefe761cc8b05437945f50
EmranHossen <emranhossen3075@gmail.com>
2025-06-13 22:29:51 +0600

    feat : Create VillaDTO and Moidfy controller

---
+++ MagicVilla_VillaAPI/Models/DTO/VillaDTO.cs
@@ -0,0 +1,11 @@
+﻿namespace MagicVilla_VillaAPI.Models.DTO
+{
+    public class VillaDTO
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public DateTime CreatedDate { get; set; }
+
+    }
+}
